3c59a4383e3f658f8e7fc107cfabc7c053178c40
Daniel Caldera <bdcaldera@gmail.com>
2023-07-26 08:54:59 +0000

    added codespace startup script

--- aws/startup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd /workspaces/ultimate/frontend-react-js && npm install
-cd /workspaces/ultimate/backend-nest-js && npm install
---
+++ scripts/codespace-startup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd $CODESPACE_VSCODE_FOLDER/frontend-react-js && npm install
+cd $CODESPACE_VSCODE_FOLDER/backend-nest-js && npm install
